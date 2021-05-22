af551e5919daea20a1dfc4a2d6ca1ea548f32705
Sahil <itssahil6@gmail.com>
2021-05-22 19:54:37 -0400

    remaking basic portfolio page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import Title from "./components/title.jsx";
+import "./styles.css";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+	<React.StrictMode>
+		<Title />
+	</React.StrictMode>,
+	document.getElementById("root")
 );
