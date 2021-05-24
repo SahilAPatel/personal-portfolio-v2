738c0d9ef45be80d24e28032615b66f2a977f881
sahil <pates22@mcmaster.ca>
2021-05-24 16:53:28 -0400

    going with functions instead of classes

---
+++ src/components/about.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+const About = () => {
+    const [skills, setSkills] = useState("");
+
+    return (
+
+    );
+}
+
+
--- src/components/title.jsx
+++ src/components/title.jsx
@@ -2,38 +2,41 @@ import React from "react"
 //import "./stars.scss"
 import Typed from "react-typed"
 
-class Title extends React.Component {
-    render() {
-        return (
-            <div id="home" className="intro route bg-image background">
-                <div className="intro-content display-table">
-                <div className="table-cell">
-                    <div className="container">
+
+const Title = () => {
+    return (
+        <div id="home" className="intro route bg-image background">
+            <div className="intro-content display-table">
+            <div className="table-cell">
+                <div className="container">
                     <h1 className="intro-title mb-4">Sahil Patel</h1>
-                    <p>I'm probably</p>
-                    <p className="intro-subtitle">
-                        <span className="text-slider-items"></span>
-                        <strong className="text-slider">
-                        <Typed
-                            strings={[
-                                "developing code",
-                                "playing videogames",
-                                "drinking coffee",
-                                "reading a book",
-                                "playing basketball",
-                                "doing leetcode",
-                                "listening to a podcast",
-                                "doing homework",
-                                "thinking about sushi",
-                                "listening to music"
-                            ]}
-                            typeSpeed={80}
-                            backDelay={1100}
-                            backSpeed={30}
-                            loop
-                        />
-                        </strong>
-                    </p>
+                    <div className="grid-container">
+                        <p className="intro-subtitle">I'm probably&nbsp;</p>
+                        <p className="intro-subtitle">
+                            <span className="text-slider-items"></span>
+                            <strong className="text-slider">
+                            <Typed
+                                strings={[
+                                    "developing code",
+                                    "playing videogames",
+                                    "drinking coffee",
+                                    "reading a book",
+                                    "playing basketball",
+                                    "doing leetcode",
+                                    "listening to a podcast",
+                                    "doing homework",
+                                    "thinking about sushi",
+                                    "listening to music"
+                                ]}
+                                typeSpeed={80}
+                                backDelay={1100}
+                                backSpeed={30}
+                                loop
+                            />
+                            </strong>
+                        </p>
+                    </div>
+
                     <p className="pt-3">
                         <a
                         className="btn btn-primary btn js-scroll px-4"
@@ -43,12 +46,12 @@ class Title extends React.Component {
                         View My Work
                         </a>
                     </p>
-                    </div>
-                </div>
                 </div>
             </div>
-        );
-    }
+            </div>
+        </div>
+    );
+
 }
 
 export default Title;
